f7863c351e8214dd1a4b32dac693805fb23121e9
Victor Lin <victor@coupa.com>
2011-04-25 18:09:28 -0700

    system()...doesn't return stdout. doh.

--- ext/xmlsec/extconf.rb
+++ ext/xmlsec/extconf.rb
@@ -3,7 +3,7 @@ if pkg_config('xmlsec1-openssl')
 	# The rightscale image appears to have incorrect
 	# pkg-config files - this flag is needed to link against an 
 	# 64-bit libxmlsec1 but isn't included in the pkg-config 
-	if system("uname -p") == 'x86_64'
+	if `uname -p`.match 'x86_64'
 		$CFLAGS += " -DXMLSEC_NO_SIZE_T"
 	end
 	create_makefile('xmlsec')
